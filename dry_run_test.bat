d01eabd287ea2d56809a0006241f935fbbc59c72
Daniel <eldanypalomo@gmail.com>
2025-11-18 23:12:17 -0600

    Clean up: remove obsolete bat files (run_hello.bat, dry_run_test.bat)

--- dry_run_test.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Testing bot in DRY RUN mode...
-"C:\Users\edgar\roboquant\venv\Scripts\python.exe" -c "print('Add DRY RUN code to execute_trade() first')"
-pause
--- run_hello.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo.
-echo 👋 Hello MT5 Test...
-echo.
-python hello_mt5.py
-echo.
-pause
